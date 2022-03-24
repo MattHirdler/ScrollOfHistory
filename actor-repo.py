355c1dde48da4e3fb82c840cfa05a2fc7e0c002c
matthirdler <mattettler@gmail.com>
2022-03-23 23:13:10 -0600

    BLUE(actor-repo) create the docstring for test_enter

--- actor-repo.py
+++ actor-repo.py
@@ -6,6 +6,7 @@ class StoreTest(unittest.TestCase):
         cls.repo = Store()
 
     def test_enter(self):
+        """test that __enter__ is available for context managers"""
         self.assertEquals(StoreTest.repo.__enter__(), StoreTest.repo)
     def test_exit(self):
         self.assertEqual(StoreTest.repo.__exit__(None, None, None), StoreTest.repo)
