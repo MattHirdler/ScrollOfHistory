94ae6c2b1b8b5e33fcf7acbbbd396e8208433119
matthirdler <mattettler@gmail.com>
2022-03-23 23:08:49 -0600

    GREEN(actor-repo): test that Store supports complete

--- actor-repo.py
+++ actor-repo.py
@@ -10,20 +10,24 @@ class StoreTest(unittest.TestCase):
     def test_exit(self):
         self.assertEqual(StoreTest.repo.__exit__(None, None, None), StoreTest.repo)
     def test_complete(self):
-        self.assertEqual(StoreTest.repo._complete == False)
+        self.assertEqual(StoreTest.repo._complete, False)
         self.assertEqual(StoreTest.repo.complete(), StoreTest.repo)
-        self.assertEqual(StoreTest.repo._complete == True)
+        self.assertEqual(StoreTest.repo._complete, True)
     def test_close(self):
         pass
 
 class Store():
     def __init__(self):
-        self.storage = []
+        self._complete = False
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        return self
+
+    def complete(self):
+        self._complete = True
         return self
 # Create
 
