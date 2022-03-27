fd3f8036fb01231ddf8f6a154c486d9f56eb29e9
matthirdler <mattettler@gmail.com>
2022-03-26 23:29:07 -0600

    RED(actor-repo): create test that ActorStore supports next()

--- actor-repo.py
+++ actor-repo.py
@@ -33,6 +33,21 @@ class ActorStoreTest(unittest.TestCase):
         yearsActive = range(year_of_birth, year_of_death)
         self.assertEqual(ActorStoreTest.repo.addActor(name, yearsActive), (name, yearsActive))
 
+    def test_next(self):
+        """Get the next actor in the repository sorted by year_of_birth descand loops back to the top when the list is exhausted"""
+        name1 = 'George Washington'
+        name2 = name1.join('2')
+        year_of_birth1 = 1940
+        year_of_death1 = year_of_birth1
+        years_active1 = range(year_of_birth1, year_of_death1)
+        year_of_birth2 = year_of_birth1+1
+        year_of_death2 = year_of_birth2
+        years_active2 = range(year_of_birth2, year_of_death2)
+        ActorStoreTest.repo.addActor(name1, years_active1)
+        ActorStoreTest.repo.addActor(name2, years_active2)
+        self.assertEqual(ActorStoreTest.repo.next(), name1, years_active1)
+        self.assertEqual(ActorStoreTest.repo.next(), name2, years_active2)
+        self.assertEqual(ActorStoreTest.repo.next(), name1, years_active1)
         
 class Store():
     """The base type of all repositories. Intended to provide a seperation between reading data-models and handling persistence. Transactions are marked as complete by calling 'complete'"""
